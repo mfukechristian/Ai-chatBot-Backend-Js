009570b3e8a5fce7a3f25447dd5a5386cb7ab91f
juliansteam <juliansteam@gamil.com>
2023-09-01 10:48:23 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const { DiscussServiceClient } = require("@google-ai/generativelanguage");
+const { GoogleAuth } = require("google-auth-library");
+
+const app = express();
+const port = 3001;
+
+const MODEL_NAME = "models/chat-bison-001";
+
+// Replace with your actual API key or set it as an environment variable
+const API_KEY = "AIzaSyC_ZAGMX6Rx8LC-asNeRFyQ5r73XimMLms";
+
+const client = new DiscussServiceClient({
+  authClient: new GoogleAuth().fromAPIKey(API_KEY),
+});
+
+app.use(express.json());
+
+app.post("/api/chat", async (req, res) => {
+  const inputMessage = req.body.message;
+
+  try {
+    let messages = [{ content: inputMessage }];
+
+    const result = await client.generateMessage({
+      model: MODEL_NAME,
+      prompt: { messages },
+    });
+
+    const response = result[0].candidates[0].content;
+
+    messages.push({ content: response });
+
+    // You can continue the conversation or generate more responses here if needed.
+
+    res.json({ response });
+  } catch (error) {
+    console.error("Error:", error);
+    res
+      .status(500)
+      .json({ error: "An error occurred while processing your request." });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
